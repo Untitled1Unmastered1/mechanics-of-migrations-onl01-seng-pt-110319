7dbb365063d0bef5240209ae7450758932a2264a
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-22 02:49:39 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,4 @@
-class CreateArtists < ActiveRecord::Migration[5.2]class CreateArtists < ActiveRecord::Migration[5.2]
+class CreateArtists < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
       t.string :name
@@ -7,16 +7,12 @@ class CreateArtists < ActiveRecord::Migration[5.2]class CreateArtists < ActiveRe
       t.string :hometown
     end
   end
-end
   
-  
-#   def up
-#   end
+    def up
+  end
  
-#   def down
-#   end
+  def down
+  end
+end
+
   
-#   def change
-#     create_table :artists do |t|
-#   end 
-# end
